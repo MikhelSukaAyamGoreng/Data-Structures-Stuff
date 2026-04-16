0597434bf593a9de93d090f76034bf4bc777df7d
MikhelSukaAyamGoreng <mikhelessien3@gmail.com>
2026-04-16 15:48:17 +0700

    learn hash table

--- Try2.c
+++ Try2.c
@@ -1,106 +1,81 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct student {
-    int score;
-    char name[1000];
-    struct student *next;
-    struct student *previous;
-}student;
+#define max 100
 
-student std;
+typedef struct node {
+    int val;
+    int key;
+    struct node *next;
+}node;
 
-// The Efficient Way
-struct student *reverse(struct Node *head) {
-    struct student *prev = NULL;
-    struct student *after = NULL;
-    struct student *current = head;
+typedef struct hash_table {
+    node **table;
+}hash_table;
 
-    while (current != NULL) {
-        after = current->next;
-        current->next = prev;
-        prev = current;
-        current = after;
-    }
-
-    return prev;
+// Hashing functions
+int hash(int target_val){
+    return target_val % max;
 }
 
-// Insertion #middle
-struct student *insertionMid (struct student **headRef, int val, char nem[]) {
-    struct student *NewNode = (struct student *)malloc(sizeof(struct student));
-    NewNode->score = val;
-    strcpy(NewNode->name, nem);
+hash_table *initializez() {
+    hash_table *newNode = (hash_table*)malloc(sizeof(hash_table));
+    newNode->table = (node**)malloc(sizeof(node*) * max);
+    for (int i = 0; i < max; i++) {
+        newNode->table[i] = NULL;
+    }
+    return newNode;
+}
+
+node *createNode(int target_val, int target_key) {
+    node *newNode = (node*)malloc(sizeof(node));
+    newNode->val = target_val;
+    newNode->key = target_key;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insert(hash_table *ht, int target_val) {
+    unsigned int target_key = hash(target_val);
+
+    node *entry = ht->table[target_key];
+    node *prev = NULL; 
+
+    while (entry != NULL) {
+        if (entry->val == target_val) {
+            
+            return;
+        }
+        prev = entry;
+        entry = entry->next;
+    }
     
-    NewNode->next = (*headRef);
-    (*headRef) = NewNode;
-}
-
-// Insertion #End
-struct student *insertion (struct student *head, int val, char nem[]) {
-    struct student *NewNode = (struct student *)malloc(sizeof(struct student)); 
-    NewNode->score = val;
-    strcpy(NewNode->name, nem);
-    NewNode->next = NULL;
-    
-    
-    struct student *current = head;
-    while (current->next != NULL) {
-        current = current->next;
-    }
-
-    current->next = NewNode;
-    return NewNode;
-}
-
-// Deletion
-struct student *deletion (struct student *head, char nem[]) {
-    if (head != NULL && strcmp(head->name, nem) == 0) {
-        struct student *temp = head;
-        head->next = head;
-        free(temp);
-        return head;
-    }
-
-    struct student *current = head;
-
-    while (current != NULL && current->next != NULL) {
-        if (strcmp(nem, current->next->name) == 0) {
-            struct student *temp = current->next;
-            current->next = current->next->next;
-            free(temp);
-            return head;
-        }   
-        current = current->next;
-    }
-
-    return head;
-}
-
-// Main Menu
-int main() {
-    int x;
-    printf("Choose menu: \n");
-    printf("1. Add new to List\n");
-    printf("2. Print List\n");
-    printf("3. Search List\n");
-    printf("4. Remove a list \n");
-
-    printf("Give the number of the menu you want to acces: ");
-    scanf("%d", &x);
-
-    if (x == 1) {
-
-    }
-    else if (x == 2) {
-
-    }
-    else if (x == 3) {
-
+    node *newNode = createNode(target_val, target_key);
+    if (prev == NULL) {
+        ht->table[target_key] = newNode;
+        return;
     }
     else {
-        printf("Plese choose the available menu \n");
+        prev->next = newNode;
+    }
+}
+
+int hash_get(hash_table *ht, int target_val) {
+    unsigned int target_key = hash(target_val);
+    node *entry = ht->table[target_key];
+    if (entry == NULL) {
+        return -1;
     }
 
+    while (entry != NULL) {
+        if (entry->val == target_val) {
+            return entry->key;
+        }
+        entry = entry->next;
+    }
+    return -1;
+}
+
+int main() {
     return 0;
 }
